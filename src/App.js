8d32b71b08bc9d85f26495e17c608625d53b8bfd
90054169Boitmann <55775047+MichaelBoitmann@users.noreply.github.com>
2022-03-03 23:03:04 +1300

    some changes for browser router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom';
 
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const HomePage = () => (
+  <div>
+    <h1>HOME PAGE</h1>
+  </div>
+)
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path='/' component={HomePage} />
     </div>
   );
 }
